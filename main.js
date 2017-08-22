f6d80f5516030728ac8841a5a5ac5f04ef5ba09e
mconstanza <michael.constanza@gmail.com>
2017-08-22 17:03:50 -0400

    added basic database connection, list of lists, list of items within list when clicked

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
+require('dotenv').config()
 const electron = require('electron')
-const {app, BrowserWindow} = electron
+const {app, BrowserWindow, ipcMain} = electron
 
 app.on('ready', () => {
   let win = new BrowserWindow({width: 1200, height: 800})
   win.loadURL(`file://${__dirname}/index.html`)
+})
+
+ipcMain.on('load-page', (event, arg) => {
+    let focusedWindow = BrowserWindow.getFocusedWindow();
+    focusedWindow.loadURL(arg.link)
+    focusedWindow.webContents.on('did-finish-load', () => {
+      focusedWindow.webContents.send('list-view', arg)
+  })
 })
